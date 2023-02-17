869c962c72f81497bbde2dbacf12615f9c3d38ef
fei-toto17 <sugarbouy17@gmail.com>
2023-02-17 11:14:39 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * a program to print lowercase alphabets
+ * return= zero(success) always
+ */
+
+int main(void)
+{
+	int ch;
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('/n');
+return(0);
+}
