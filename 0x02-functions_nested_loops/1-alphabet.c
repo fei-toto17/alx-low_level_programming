f8670ae9c8b86c07c6209da4a0ad8eba92aeb4fd
fei-toto17 <sugarbouy17@gmail.com>
2023-02-21 03:18:36 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main-entry point
+ * prints alphabets is lowercase,
+ * followed by a new line
+ * return 0
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar("\n");
+}
