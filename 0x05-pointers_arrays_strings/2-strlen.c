f79f9b980d83a7fdaa7343bec673cb15482634b9
fei-toto17 <sugarbouy17@gmail.com>
2023-02-27 07:16:36 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @s: string to check
+ *
+ * Return: int length of the string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	while (*s != '\0')
+	{
+		length += 1;
+		*s += 1;
+	}
+	return (length);
+}
