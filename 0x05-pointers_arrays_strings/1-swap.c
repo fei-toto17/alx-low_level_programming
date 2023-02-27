b3ed876c11a64b183b54e60a69cdc56c16c59805
fei-toto17 <sugarbouy17@gmail.com>
2023-02-27 07:02:42 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swat_int-> given two parameters
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
